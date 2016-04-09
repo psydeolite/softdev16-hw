2a03c6ea99a73abf5791894e8a36265ce7501d65
masha <maryged@gmail.com>
2016-04-08 21:35:50 -0400

    work fri

---
+++ hw09/rap.py
@@ -0,0 +1,25 @@
+import time
+
+def wrapper(f):
+    def inner(*arg):
+        ti=time.time()
+        r=f(*arg)
+        tf=time.time()
+        name=f.func_name
+        print "Time taken for function ["+name+"] is " + str(tf-ti)
+        return r
+    return inner
+        
+def helloworld():
+    a=0
+    for x in range(6):
+        a+=x
+    return a
+
+@wrapper
+def executioner():
+    return helloworld()
+
+test=executioner()
+print test
+
